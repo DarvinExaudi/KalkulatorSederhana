2654aa0598f7fbb6ecb8a9b1f9454141b0613cc0
Planktonot <pocongterbalik6@gmail.com>
2023-09-11 08:04:14 +0700

    Commit

---
+++ kalkusederhana/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace kalkusederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Min = new System.Windows.Forms.Button();
+            this.Kali = new System.Windows.Forms.Button();
+            this.Bagi = new System.Windows.Forms.Button();
+            this.Hapus = new System.Windows.Forms.Button();
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.button0 = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(128, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(228, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 68);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(30, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 68);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(128, 222);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 68);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(228, 222);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 68);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(30, 308);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 68);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(128, 308);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 68);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(228, 308);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 68);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(331, 136);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(80, 68);
+            this.Plus.TabIndex = 9;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // Min
+            // 
+            this.Min.Location = new System.Drawing.Point(427, 136);
+            this.Min.Name = "Min";
+            this.Min.Size = new System.Drawing.Size(80, 68);
+            this.Min.TabIndex = 10;
+            this.Min.Text = "-";
+            this.Min.UseVisualStyleBackColor = true;
+            this.Min.Click += new System.EventHandler(this.Min_Click);
+            // 
+            // Kali
+            // 
+            this.Kali.Location = new System.Drawing.Point(331, 222);
+            this.Kali.Name = "Kali";
+            this.Kali.Size = new System.Drawing.Size(80, 68);
+            this.Kali.TabIndex = 11;
+            this.Kali.Text = "*";
+            this.Kali.UseVisualStyleBackColor = true;
+            this.Kali.Click += new System.EventHandler(this.Kali_Click);
+            // 
+            // Bagi
+            // 
+            this.Bagi.Location = new System.Drawing.Point(427, 222);
+            this.Bagi.Name = "Bagi";
+            this.Bagi.Size = new System.Drawing.Size(80, 68);
+            this.Bagi.TabIndex = 12;
+            this.Bagi.Text = "/";
+            this.Bagi.UseVisualStyleBackColor = true;
+            this.Bagi.Click += new System.EventHandler(this.Bagi_Click);
+            // 
+            // Hapus
+            // 
+            this.Hapus.Location = new System.Drawing.Point(427, 308);
+            this.Hapus.Name = "Hapus";
+            this.Hapus.Size = new System.Drawing.Size(80, 68);
+            this.Hapus.TabIndex = 13;
+            this.Hapus.Text = "Clear";
+            this.Hapus.UseVisualStyleBackColor = true;
+            this.Hapus.Click += new System.EventHandler(this.Hapus_Click);
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(30, 25);
+            this.TxtHasil.Multiline = true;
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.Size = new System.Drawing.Size(278, 78);
+            this.TxtHasil.TabIndex = 14;
+            this.TxtHasil.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(331, 308);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(80, 68);
+            this.button0.TabIndex = 15;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // Equal
+            // 
+            this.Equal.Location = new System.Drawing.Point(331, 35);
+            this.Equal.Name = "Equal";
+            this.Equal.Size = new System.Drawing.Size(176, 68);
+            this.Equal.TabIndex = 16;
+            this.Equal.Text = "=";
+            this.Equal.UseVisualStyleBackColor = true;
+            this.Equal.Click += new System.EventHandler(this.Equal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 408);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.Hapus);
+            this.Controls.Add(this.Bagi);
+            this.Controls.Add(this.Kali);
+            this.Controls.Add(this.Min);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Min;
+        private System.Windows.Forms.Button Kali;
+        private System.Windows.Forms.Button Bagi;
+        private System.Windows.Forms.Button Hapus;
+        private System.Windows.Forms.TextBox TxtHasil;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button Equal;
+    }
+}
+
---
+++ kalkusederhana/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kalkusederhana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Total;
+        int num1;
+        int num2;
+        string option;
+        int result;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button7.Text;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button0.Text;
+        }
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            num2 = int.Parse(TxtHasil.Text);
+
+            if (option.Equals("+"))
+                result = num1 + num2;
+
+            if (option.Equals("-"))
+                result = num1 - num2;
+
+            if (option.Equals("*"))
+                result = num1 * num2;
+
+            if (option.Equals("/"))
+                result = num1 / num2;
+
+            TxtHasil.Text = result + "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button6.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = TxtHasil.Text + button9.Text;
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            option = "+";
+            num1 = int.Parse(TxtHasil.Text);
+
+            TxtHasil.Clear();
+        }
+
+        private void Min_Click(object sender, EventArgs e)
+        {
+            option = "-";
+            num1 = int.Parse(TxtHasil.Text);
+
+            TxtHasil.Clear();
+        }
+
+        private void Kali_Click(object sender, EventArgs e)
+        {
+            option = "*";
+            num1 = int.Parse(TxtHasil.Text);
+
+            TxtHasil.Clear();
+        }
+
+        private void Bagi_Click(object sender, EventArgs e)
+        {
+            option = "/";
+            num1 = int.Parse(TxtHasil.Text);
+
+            TxtHasil.Clear();
+        }
+
+        private void Hapus_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Clear();
+            result = (0);
+            num1 = (0);
+            num2 = (0);
+        }
+    }
+}
